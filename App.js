6b2f718bcaa4b73e4a0dc650709e9ea0a4291b4a
prithvi2607 <prc007062@gmail.com>
2020-08-10 19:14:12 +0530

    practice

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import IndexScreen from './src/screens/IndexScreen';
+import ShowScreen from './src/screens/ShowScreen';
+import CreateScreen from './src/screens/CreateScreen';
+import EditScreen from './src/screens/EditScreen';
+import { Provider } from './src/context/BlogContext';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const navigator = createStackNavigator({
+  Index: IndexScreen,
+  Show: ShowScreen,
+  Create: CreateScreen,
+  Edit: EditScreen
+}, {
+  initialRouteName: 'Index',
+  defaultNavigationOptions: {
+    title: 'Blogs'
+  }
 });
+
+const App = createAppContainer(navigator);
+
+export default () => {
+  return <Provider>
+    <App />
+  </Provider>
+};
+
+// import React from "react";
+// import { Text, StyleSheet, View, Button } from "react-native";
+
+// const Screen = () => {
+//     return <View>
+//        <Text>Hello</Text> 
+//     </View>
+// };
+
+// const styles = StyleSheet.create({});
+
+// export default Screen;
---
+++ src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'http://26d064ca5dcb.ngrok.io'
+});
